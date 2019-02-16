6e381c471b3bf028287476b4c8e18b37c1f1747a
unknown <codeleap@outlook.com>
2019-02-16 15:04:24 +0100

    Added post-content to the loop example 3.

--- 3-the-loop-get-post-meta.php
+++ 3-the-loop-get-post-meta.php
@@ -1,39 +1,38 @@
 <?php
 /*
 In this example, we show how you can access post meta during the WORDPRESS LOOP.
-We then place the title, content and post meta into an array for each post.
-Then we add this to a 'master array' which we can then localize into a js file or use the data for bespoke front end creation.
 */
 
-//Here we have the Wordpress loop found in archive pages and the index page.
-
-/* Start the Loop */
-    //Here we set our master array - it will be an array of arrays for each post
-    $master_array = [];
+    //Set an array to hold arrays of post details.
+    $array_of_post_details = [];
+    //Start the loop (found in archive pages, index page)
     while ( have_posts() ) :
         //the_post() acts as an iterator, looping through each post
         the_post();
+
         //We make $post obj global so we can access properties
         global $post;
-        //Here we get the id/
-        $id = $post->ID;
+
+        //We set an array for this post
+        $this_post = [];
         
-        //We create an associative array for this post
-        $this_arr = [];
-        $this_arr['id'] = $id;
-        $this_arr['title'] = $post->post_title;
+        //Add id, post title and post content to array
+        $this_post['id'] = $post->ID;
+        $this_post['title'] = $post->post_title;
+        $this_post['content'] = $post->post_content;
+
         //Here we get the meta data for the keys we want.
-        $this_arr['our_promise'] = get_post_meta($id, 'our_promise', true);
-        $this_arr['price_from'] = get_post_meta($id, 'price_from', true);
-        $this_arr['content'] = $content;
-        //We add this_arr to our master array.
-        $master_array[] = $this_arr;
+        $this_post['our_promise'] = get_post_meta($id, 'our_promise', true);
+        $this_post['price_from'] = get_post_meta($id, 'price_from', true);
+
+        //We add $this_post to our master array.
+        $array_of_post_details[] = $this_post;
             
         
-        //get_template_part( 'template-parts/post/content', get_post_format() );
+        //Instead of calling get_template_part() here, we can create a bespoke archive page or unique index page.
 
         endwhile;
 
 /*
-HERE we can now use our $master_array for various bespoke page creation.
+Now we can use our $array_of_post_details array for bespoke page creation.
 */
