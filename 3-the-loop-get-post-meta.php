a2915023fecf5379ceb26876de4c3d8008a55c3d
unknown <codeleap@outlook.com>
2019-03-10 10:13:55 +0100

    Wordpress data validation addded.

--- 3-the-loop-get-post-meta.php
+++ 3-the-loop-get-post-meta.php
@@ -14,16 +14,16 @@ while ( have_posts() ) :
 
     $this_post = []; //We set an array for this post
         
-    $this_post['id'] = $post->ID;
-    $this_post['title'] = $post->post_title;
-    $this_post['content'] = $post->post_content;
-
+    $this_post['id'] = esc_html( $post->ID );
+    $this_post['title'] = esc_html( $post->post_title );
+    $this_post['content'] = esc_html( $post->post_content ) ;
+ 
     //Get meta data and add to our array
-    $this_post['our_promise'] = get_post_meta($id, 'our_promise', true);
-    $this_post['price_from'] = get_post_meta($id, 'price_from', true);
+    $this_post['our_promise'] = esc_html( get_post_meta($id, 'our_promise', true ));
+    $this_post['price_from'] = esc_html( get_post_meta($id, 'price_from', true ));
 
     $array_of_post_details[] = $this_post; //Add $this_post to our master array.
-             
-    //Instead of calling get_template_part() here, we can create a bespoke archive page or unique index page.
 
 endwhile;
+
+//Instead of calling get_template_part() here, we can create a bespoke archive page or unique index page.
