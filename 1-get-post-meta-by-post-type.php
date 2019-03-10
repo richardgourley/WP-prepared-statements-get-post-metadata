4d87b776c6c2477c5f301fc91df5121e47cd71b8
unknown <codeleap@outlook.com>
2019-03-10 10:03:09 +0100

    Updated string sanitization using Wordpress data vaildation methods.

--- 1-get-post-meta-by-post-type.php
+++ 1-get-post-meta-by-post-type.php
@@ -1,35 +1,47 @@
 <?php
-$post_type = "services_offered"; //Example variable from user input.
 
 /*
-* Loop through posts for a given post type, return an HTML string with details for each post.
+* Search for posts for a given type, return an HTML string with details for each post.
 * 
-* @ param $post_type is any selected post type to be looped.
+* @ param $post_type 
+* @ param $meta_data is the meta data required
 * @ return string: a string containing HTML.
-* 
-* NOTE: %s signifies a string in prepared statements.
-* NOTE: You can use %d for integers, %f for floats in your prepared statements.
 */
-function get_metadata_by_post_type($post_type){
-    $post_type_sanitized = filter_var($post_type, FILTER_SANITIZE_STRING); //Sanitize input parameter.
+
+$post_type = 'services_offered';
+$meta_keys = array( 'price_from', 'our_promise' );
+
+function get_post_data_metadata($post_type, $meta_keys){
+    $post_type_sanitized = esc_sql( $post_type ); // Escape for sql query. Not strictly necessary as using $wpdb->preprare()
 
     $output_string = "";
     global $wpdb;
     
+    // NOTE: %s signifies a string in prepared statements.
+    // NOTE: You can use %d for integers, %f for floats in your prepared statements.
     $stmt = $wpdb->prepare(
         "SELECT * FROM wp_posts WHERE post_type = %s", 
         $post_type_sanitized
     );
     
-    $results = $wpdb->get_results($stmt); //Returns array of objects
+    $results = $wpdb->get_results( $stmt ); //Returns array of objects
 
     foreach($results as $entry){
-        $output_string .= "<h1>" . htmlspecialchars($entry->post_title) . "</h1>"; //Escape all output 
+        $output_string .= "<h1>" . esc_html( $entry->post_title ) . "</h1>"; //Escape all output 
         $output_string .= "<p>PRICES FROM: $";
         //TRUE parameter in get_post_meta() means you expect a single result.
-        $output_string .=  htmlspecialchars(get_post_meta($entry->ID, 'price_from', true)) . "</p>"; 
-        $output_string .= "<p>" . htmlspecialchars(get_post_meta($entry->ID, 'our_promise', true)) . "</p>";
+        foreach($meta_keys as $meta_key){
+            $meta_key_cleaned = esc_html($meta_key);
+            $output_string .= "<p>" . esc_html( get_post_meta( $entry->ID, $meta_key_cleaned, true ));
+        }
+    
     }
     
     return $output_string;
 }
+
+/* 
+* This is ONLY intended as a simple example of using $wpdb->prepare
+* Use $wpdb for more bespoke queries using joins, inner joins etc.
+* Use $query = new WPQuery( $args ) for basic queries like this!
+*/
