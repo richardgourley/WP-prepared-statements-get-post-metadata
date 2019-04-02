1a006e67a7646cc956ab161bf31e512282e96e51
unknown <codeleap@outlook.com>
2019-04-02 14:46:58 +0200

    Added a more advanced example using wp_posts, wp_terms and wp_term_relationships with wpdb prepared statements.

--- 1-wpdb-prepared-statements-example.php
+++ 1-wpdb-prepared-statements-example.php
@@ -5,52 +5,31 @@
 * Use $wpdb when you needto create more advanced queries often using multiple tables with joins.
 */
 
+class PostQuantityCounter{
+    /*
+    * @ no params
+    * returns array - joins 3 tables to find how many posts there are for each term type
+    * 'custom_post_type' is example of potential user input - made safe with prepared statements.
+    */
 
-/*
-*/
+    public function count_posts_per_category(){
+        global $wpdb;
+        $query = $wpdb->prepare( 
+            "SELECT COUNT(ID), name
+            FROM wp_posts, wp_terms, wp_term_relationships
+            WHERE wp_posts.post_type != %s
+            AND wp_posts.ID = wp_term_relationships.object_id
+            AND wp_terms.term_id = wp_term_relationships.term_taxonomy_id
+            GROUP BY wp_terms.name",
+            'custom_post_type' 
+        );
+        $num_posts_per_category = $wpdb->get_results( $query ); //get array of results
 
-/*
-* Search for posts for a given type, return an HTML string with details for each post.
-* 
-* @ param $post_type 
-* @ param $meta_data is the meta data required
-* @ return string: a string containing HTML.
-*/
+        return $num_posts_per_category;
 
-$post_type = 'services_offered';
-$meta_keys = array( 'price_from', 'our_promise' );
-
-function get_post_data_metadata($post_type, $meta_keys){
-    $post_type_sanitized = esc_sql( $post_type ); // Escape for sql query. Not strictly necessary as using $wpdb->preprare()
-
-    $output_string = "";
-    global $wpdb;
-    
-    // NOTE: %s signifies a string in prepared statements.
-    // NOTE: You can use %d for integers, %f for floats in your prepared statements.
-    $stmt = $wpdb->prepare(
-        "SELECT * FROM wp_posts WHERE post_type = %s", 
-        $post_type_sanitized
-    );
-    
-    $results = $wpdb->get_results( $stmt ); //Returns array of objects
-
-    foreach($results as $entry){
-        $output_string .= "<h1>" . esc_html( $entry->post_title ) . "</h1>"; //Escape all output 
-        $output_string .= "<p>PRICES FROM: $";
-        //TRUE parameter in get_post_meta() means you expect a single result.
-        foreach($meta_keys as $meta_key){
-            $meta_key_cleaned = esc_html($meta_key);
-            $output_string .= "<p>" . esc_html( get_post_meta( $entry->ID, $meta_key_cleaned, true ));
-        }
-    
     }
-    
-    return $output_string;
+
 }
 
-/* 
-* This is ONLY intended as a simple example of using $wpdb->prepare
-* Use $wpdb for more bespoke queries using joins, inner joins etc.
-* Use $query = new WPQuery( $args ) for basic queries like this!
-*/
+$post_quantity_counter = new PostQuantityCounter();
+$result = $post_quantity_counter->count_posts_per_category(); //returns array showing how many IDs match each term.
